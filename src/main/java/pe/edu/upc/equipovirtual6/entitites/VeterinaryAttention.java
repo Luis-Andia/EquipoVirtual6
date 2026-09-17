a4ccf7f0e27bc2897700f17f003e5c1e77aba28b
Luis_AS <u202417048@upc.edu.pe>
2026-09-17 10:39:56 -0500

    Entidad veterinary

--- src/main/java/pe/edu/upc/equipovirtual6/entitites/VeterinaryAttention.java
+++ src/main/java/pe/edu/upc/equipovirtual6/entitites/VeterinaryAttention.java
@@ -1,7 +1,105 @@
 package pe.edu.upc.equipovirtual6.entitites;
 
+import java.time.LocalDate;
+
 public class VeterinaryAttention {
     private Long id;
     private String petName;
     private String ownerName;
+    private String description;
+    private String veterinarian;
+    private LocalDate attentionDate;
+    private String diagnosis;
+    private Double cost;
+    private String status;
+
+    // Constructores
+    public VeterinaryAttention() {
+    }
+
+    public VeterinaryAttention(Long id, String petName, String ownerName, String description, String veterinarian, LocalDate attentionDate, String diagnosis, Double cost, String status) {
+        this.id = id;
+        this.petName = petName;
+        this.ownerName = ownerName;
+        this.description = description;
+        this.veterinarian = veterinarian;
+        this.attentionDate = attentionDate;
+        this.diagnosis = diagnosis;
+        this.cost = cost;
+        this.status = status;
+    }
+
+    // Get y set
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVeterinarian() {
+        return veterinarian;
+    }
+
+    public void setVeterinarian(String veterinarian) {
+        this.veterinarian = veterinarian;
+    }
+
+    public LocalDate getAttentionDate() {
+        return attentionDate;
+    }
+
+    public void setAttentionDate(LocalDate attentionDate) {
+        this.attentionDate = attentionDate;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
