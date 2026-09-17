40a7fc5bec0f8accc425c29fd71f038a8df427f2
Luis_AS <u202417048@upc.edu.pe>
2026-09-17 10:27:53 -0500

    Creacion Proyecto

---
+++ src/main/java/pe/edu/upc/equipovirtual6/EquipoVirtual6Application.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.equipovirtual6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipoVirtual6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquipoVirtual6Application.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/equipovirtual6/EquipoVirtual6ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.equipovirtual6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipoVirtual6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
