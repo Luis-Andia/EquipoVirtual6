97950c947aa5aede2748376446dcd025cc4a14cf
Luis_AS <u202417048@upc.edu.pe>
2026-09-17 11:17:53 -0500

    Implements veternary attention 2

--- src/main/java/pe/edu/upc/equipovirtual6/servicesinterfaces/VeterinaryAttentionService.java
+++ src/main/java/pe/edu/upc/equipovirtual6/servicesinterfaces/VeterinaryAttentionService.java
@@ -3,5 +3,5 @@ package pe.edu.upc.equipovirtual6.servicesinterfaces;
 import pe.edu.upc.equipovirtual6.entitites.VeterinaryAttention;
 
 public interface VeterinaryAttentionService {
-    public void insert(VeterinaryAttention vA);
+    public void insert(VeterinaryAttention vAe6);
 }
